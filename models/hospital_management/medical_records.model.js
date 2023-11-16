178c883303ef56a8ffafefe8240cf055f0810323
Muhammad Adnan <79781004+Muhammad-Adnan796@users.noreply.github.com>
2023-11-17 01:45:23 +0500

    Delete models directory

--- models/hospital_management/medical_records.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordsSchema = new mongoose.Schema({
-
-},{timestamps:true});
-
-export const MedicalRecord = mongoose.model("MedicalRecord",medicalRecordsSchema);
