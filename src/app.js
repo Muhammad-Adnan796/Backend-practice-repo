8acc36bc8322def43c5e4480029f4167e2f54da3
Muhammad-Adnan796 <adnan108123796@gmail.com>
2023-11-21 00:34:37 +0500

    routes and setup

--- src/app.js
+++ src/app.js
@@ -12,6 +12,39 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
+
+// Router Imports
+
+import  userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
+
+/* app: ===> In the context of Node.js and Express.js, 
+app refers to an instance of the Express application. 
+It's the central part where you configure routes, middleware,
+ and other functionalities for your web application.
+ */
+
+/* use: ===> This is a method in Express.js used to mount middleware. 
+ Middleware functions in Express.js have access to the request, response, 
+ and the next function in the application's request-response cycle. 
+ This method is used to add middleware functions to the middleware stack. 
+ */
+
+/*  express.static:  ===> This is a built-in middleware function in Express.js. 
+ It serves static files, such as HTML, CSS, images, and JavaScript files,
+   from a directory. It takes a directory path as an argument and, 
+ when a request is made to the server, 
+ looks for the requested file within that directory to serve it.
+  */
+
+/* "public": ===> This is the argument passed to express.static. 
+ It represents the directory from which the static files will be served. 
+ In this case, it's specifying that the server should look for static 
+ files in a directory named "public".
+  */
