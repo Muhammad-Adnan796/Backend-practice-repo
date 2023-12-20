46bd4b94f091a5ea061e2285a44f94b5c0640f46
Muhammad-Adnan796 <adnan108123796@gmail.com>
2023-12-21 01:49:31 +0500

    removed consolelog

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,14 +5,11 @@ import { User } from "../models/user.model.js";
 
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
-  console.log("token")
   try {
-    console.log("token")
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-      console.log("token",token)
     if (!token) {
       throw ApiError(401, "Unauthorized request!");
     }
